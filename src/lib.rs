f039fbda67a520e78ab4b2f4b0a9e8c9ae16f878
Сухарик <65870+suhr@users.noreply.github.com>
2022-06-14 15:51:29 +0300

    TODO

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,27 @@ macro_rules! r {($r:expr; $x:pat,$o:expr; $y:pat,$e:expr) => (match $r {Ok($x)=>
 macro_rules! a {($a:expr) => (assert!($a))}
 macro_rules! ae {($l:expr, $r:expr) => (assert_eq!($l,$r))}
 macro_rules! l {($($x:pat,$v:expr),+; $($t:expr);+) => ({$(let $x = $v);+; $($t);+})}
+macro_rules! m {($e:expr) => (&mut $e)}
 
-macro_rules! i32 {($n:expr) => ($n as i32)}
-macro_rules! u32 {($n:expr) => ($n as u32)}
-macro_rules! uz {($n:expr) => ($n as uz)}
+macro_rules! u8  {($n:expr) => ($n as u8) }  macro_rules! i8  {($n:expr) => ($n as i8) }
+macro_rules! u16 {($n:expr) => ($n as u16)}  macro_rules! i16 {($n:expr) => ($n as i16)}
+macro_rules! u32 {($n:expr) => ($n as u32)}  macro_rules! i32 {($n:expr) => ($n as i32)}
+macro_rules! u64 {($n:expr) => ($n as u64)}  macro_rules! i64 {($n:expr) => ($n as i64)}
+macro_rules! uz  {($n:expr) => ($n as uz) }  macro_rules! iz  {($n:expr) => ($n as uz) }
 
-macro_rules! mr {
-    ($n:ident, $s:ident; $($a:ident:$t:ty),+; $r:ty $b:block) =>
-        (fn $n(&self, $($a:$t),+)-> $r {let $s=self; $b});
-}
-macro_rules! mm {
-    ($n:ident, $s:ident; $($a:ident:$t:ty),+; $r:ty $b:block) =>
-        (fn $n(&mut self, $($a:$t),+)-> $r {let $s=self; $b});
-}
+macro_rules! mr
+  { ($n:ident, $s:ident; $($a:ident:$t:ty),+; $r:ty $b:block) =>
+        (fn $n(&self, $($a:$t),+)-> $r {let $s=self; $b});          }
+macro_rules! mm
+  { ($($n:ident($s:ident, $($a:ident:$t:ty),+) $r:ty $b:block)+) =>
+        ($(fn $n(&mut self, $($a:$t),+)-> $r {let $s=self; $b})+);  }
+
+fn u32bm(b: &mut[u32])-> &mut[u8]
+  { unsafe {std::slice::from_raw_parts_mut(b.as_mut_ptr() as *mut _, 4*b.len())} }
+fn cy<T:Copy>(d: &mut[T], s:&[T])-> uz
+  { l![l,d.len().min(s.len());  d[..l].copy_from_slice(&s[..l]);  l]             }
+
+fn cduz(l:uz, r:uz)-> uz {l/r + uz!(l%r != 0)}
 
 const T:bool = true;  const F:bool = false;
 #[allow(non_camel_case_types)] type uz = usize;
@@ -31,16 +39,11 @@ const T:bool = true;  const F:bool = false;
 pub struct Ar {d:Vec<u32>, h:Vec<u32>}
 
 impl Ar {
-    mm!{al,s; z:u32; (u32, &mut[u32]) {
-        l![l,s.d.len(); s.d.resize(l+uz!(z), 0); (u32!(l), &mut s.d[l..])]
-    }}
+mm!{
+    al(s, z:uz) (u32, &mut[u32]) {l![l,s.d.len();  s.d.resize(l+z, 0);  (u32!(l), m![s.d[l..]])]}
+}
 }
 
-pub fn mr(a: &mut Ar, s: &[u8], e: &[u8])-> u32 {
-    let l = l![l,s.len();  a!(l==e.len() && l<0x100000);  u32!(l)];
-    let (r,d) = a.al((2*l+3)/4 + u32![(2*l+3)%4 != 0]);
-    d[0] = u32!(Nk::R) + u32!(l)<<4 + b![l==0; 0; u32!(s[0])]<<24;
-    todo!("copy bytes");
-
-    r
-}
+pub fn mr(a: &mut Ar, s: &[u8], e: &[u8])-> u32
+  { let l = s.len();  a!(l==e.len() && l<0x10000000);  let (r,d) = a.al(cduz(2*l+3, 4));
+    d[0] = u32!(Nk::R) + u32!(l)<<4;  l![d,u32bm(m!(d[1..])),  l,cy(d,s);  cy(m!(d[l..]),e)];  r }
