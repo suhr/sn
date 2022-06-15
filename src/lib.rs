4c347636663d7fc695b59e8ce3d349fb046c48f4
Сухарик <65870+suhr@users.noreply.github.com>
2022-06-15 18:42:42 +0300

    TODO

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::hash::{BuildHasher, Hasher};
+
 type O<T> = Option<T>;  type R<T,E> = Result<T,E>;
 
 macro_rules! b {($c:expr; $t:expr; $e:expr)               => (if $c {$t} else {$e})              }
@@ -21,6 +23,8 @@ macro_rules! mm
   { ($($n:ident($s:ident, $($a:ident:$t:ty),+) $r:ty $b:block)+) =>
         ($(fn $n(&mut self, $($a:$t),+)-> $r {let $s=self; $b})+);  }
 
+fn u32b(b: &[u32])-> &[u8]
+  { unsafe {std::slice::from_raw_parts(b.as_ptr() as *const _, 4*b.len())}       }
 fn u32bm(b: &mut[u32])-> &mut[u8]
   { unsafe {std::slice::from_raw_parts_mut(b.as_mut_ptr() as *mut _, 4*b.len())} }
 fn cy<T:Copy>(d: &mut[T], s:&[T])-> uz
@@ -36,21 +40,24 @@ const T:bool = true;  const F:bool = false;
 
 #[derive(Clone,Copy,PartialEq,Eq,PartialOrd,Ord)] enum Nk {R=0,S, C,K, O,A,N, V}
 
-pub struct Ar {d:Vec<u32>, h:Vec<u32>}
+pub struct Ar {d:Vec<u32>, h:Box<[u32]>, hl:uz, r: ahash::RandomState}
 
 impl Ar {
 mm!{
     al(s, p:uz) (u32, &mut[u32])
       { a!(p<0x10000000);  l![l,s.d.len();  s.d.resize(l+1+p, 0);  (u32!(l), m![s.d, l..])] }
+    rg(s, i:u32, d:&[u32]) ()
+      { let mut h = s.r.build_hasher();  h.write(u32b(d));  let i = uz![h.finish()] % s.h.len();
+        todo!() }
 }
 }
 
 pub fn mr(a: &mut Ar, s: &[u8], e: &[u8])-> u32
   { let l = s.len();  a!(l==e.len());  let (r,d) = a.al(cduz(2*l, 4));
-    d[0] = u32!(Nk::R) + u32!(l)<<4;  l![d,u32bm(m![d,1..]), l,cy(d,s); cy(m![d,l..], e)];  r }
+    d[0] = u32!(Nk::R) + u32!(l)<<4;  l![d,u32bm(m![d,1..]); cy(d,s); cy(m![d,l..], e)];  r }
 pub fn ms(a: &mut Ar, s: &[u8])-> u32
   { let l = s.len();  let (r,d) = a.al(cduz(l,4));
-    d[0] = u32!(Nk::S) + u32!(l)<<4;  l![d,u32bm(m![d,1..]); cy(d,s)];  r                     }
+    d[0] = u32!(Nk::S) + u32!(l)<<4;  l![d,u32bm(m![d,1..]); cy(d,s)];  r                   }
 
 fn m1(a: &mut Ar, k:Nk, c:u32)-> u32
   { a!(c<0x10000000);  let (r,d) = a.al(0);  d[0] = u32!(k) + u32!(c)<<4;  r                    }
